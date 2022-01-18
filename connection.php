acdc15395b1fe0d0d40417311c7dafea0a1052e7
Rarazu <rara_zahra_29rpl@student.smktelkom-mlg.sch.id>
2022-01-18 10:01:00 +0700

    upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+// file ini digunakan untuk menghubungkan ke database
+$connect = mysqli_connect("localhost","root","","laundry");
+// localhost = sebagai hostname
+// root = sebagai username untuk masuk ke mysql
+// kosong = password untuk masuk ke mysql
+// laundry = nama database yg kita hubungkan
+?>
